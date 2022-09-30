4863790fce2199fd5a5bc22d627525cfe91bcf55
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-09-30 12:31:38 +0530

    Add files via upload

---
+++ Introduction to java activity 1/myprogram.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class myprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("My first java program");
+	}
+
+}

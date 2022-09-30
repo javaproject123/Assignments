65a927a4aa99ee155c3591fd34145e7f6b9e9047
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-09-30 12:45:44 +0530

    Add files via upload

---
+++ java activity 2/Areacalculator.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Areacalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle r = new Rectangle();
+		r.length = 30;
+		r.breadth = 20;
+		r.calculateArea();
+		System.out.println(r.calculator);
+	}
+
+}
